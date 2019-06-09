8f8266e8235def45c64106d5f389327fcf4069e4
Heena <heenaj198@gmail.com>
2019-06-09 21:54:09 +0530

    added number parser

--- json.js
+++ json.js
@@ -7,3 +7,57 @@ function boolParser (input) {
   if (input.startsWith('false')) return [false, input.slice(5)]
   return null
 }
+function numParser (input) {
+  let _string = ''
+  let i = 0
+  if ((input[i] === '-') && (input[i + 1] >= '0' && input[i + 1] <= '9')) {
+    _string = input[0]
+    i++
+  }
+  if (input[i] === '0') {
+    _string += input[i]
+    i++
+    if (input[i] === '.' && (input[i + 1] >= '0' && input[i + 1] <= '9')) {
+      _string += input[i]
+      i++
+      while (input[i] >= '0' && input[i] <= '9') {
+        _string += input[i]
+        i++
+      }
+    }
+  } else if (input[i] >= '1' && input[i] <= '9') {
+    _string += input[i]
+    i++
+    while (input[i] >= '0' && input[i] <= '9') {
+      _string += input[i]
+      i++
+    }
+    if (input[i] === '.' && (input[i + 1] >= '0' && input[i + 1] <= '9')) {
+      _string += '.'
+      i++
+      while (input[i] >= '0' && input[i] <= '9') {
+        _string += input[i]
+        i++
+      }
+    }
+  }
+  if (input[i] === 'e' || input[i] === 'E') {
+    if ((input[i + 1] === '-' || input[i + 1] === '+') || (input[i + 1] >= '0' && input[i + 1] <= '9')) {
+      if (input[i + 1] === '-' || input[i + 1] === '+') {
+        if (input[i + 2] >= '0' && input[i + 2] <= '9') {
+          _string += input[i] + input[i + 1] + input[i + 2]
+          i += 3
+        }
+      } else {
+        _string += input[i] + input[i + 1]
+        i += 2
+      }
+    }
+  }
+  while (input[i] >= '0' && input[i] <= '9') {
+    _string += input[i]
+    i++
+  }
+  if (_string.length === 0) return null
+  return [Number(_string), input.slice(_string.length)]
+}
